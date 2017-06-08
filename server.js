c2c16a7fb6052ed4f74153588bd47cb02abba44e
Abdullah <abdullah.nasim@netaq.ae>
2017-06-08 09:32:14 +0400

    Added throng for concurrency on server.

--- server.js
+++ server.js
@@ -1,33 +1,46 @@
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
 
-var app = express();
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
 
-//Body Parser Middleware
-app.use(bodyParser.json());
 
-//Express Validator Middleware
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+function start() {
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+  var express = require('express');
+  var bodyParser = require('body-parser');
+  var expressValidator = require('express-validator');
+
+  var app = express();
+
+  //Body Parser Middleware
+  app.use(bodyParser.json());
+
+  //Express Validator Middleware
+  app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  }));
 
-require('./routes/users.js')(app);
+  require('./routes/users.js')(app);
 
-app.listen(process.env.PORT || 5000, function(){
-	console.log('Server Started at port 5000');
-});
+  app.listen(process.env.PORT || 5000, function(){
+    console.log('Server Started at port 5000');
+  });
+  
+}
